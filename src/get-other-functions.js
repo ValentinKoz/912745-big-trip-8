db52679157345a617e473d0d3b5255d7269f6870
Valentin Kozhin <kozhin_val@mail.ru>
2019-03-06 06:18:21 +0300

    Личный проект: И ехать и шашечки

---
+++ src/get-other-functions.js
@@ -0,0 +1,42 @@
+const Filters = [`Everything`, `Future`, `Past`];
+const TEST_DATA = 7;
+
+const rand = (max = 6, min = 1) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const generIcons = (data) => {
+  const typeIcons = Array.from(data.routeType);
+  return typeIcons[rand(9, 0)];
+};
+
+const getRandomOffers = (data) => {
+  const QUTY_OFFERS = rand(3, 0);
+  const setFromArray = Array.from(data.offers);
+  const offers = new Set([]);
+
+  while (offers.size < QUTY_OFFERS) {
+    const randomOffer = setFromArray[rand(4, 0)];
+    offers.add(randomOffer);
+  }
+  return [...offers].map((it) => `<li>
+    <button class="trip-point__offer">${it} +&euro; ${rand(250, 15)}</button> </li>`).join(` `);
+};
+
+const startTrip = (data) => {
+  const formatDate = new Date(data.dateTrip);
+  const dateTime = formatDate.toString().split(` `);
+  return dateTime[4].split(`:`, 2).join(`:`);
+};
+
+const finishAndDuration = (data) => {
+  const durationPerMinutes = rand(10000000, 10000);
+  const duration = (durationPerMinutes / 60000).toFixed();
+  const durationHours = Math.floor(duration / 60) + `h ` + duration % 60 + `m`;
+
+  const timeOfArrival = new Date(data.dateTrip + durationPerMinutes).toString().split(` `);
+  const timeFinish = timeOfArrival[4].split(`:`, 2).join(`:`);
+  return [durationHours, timeFinish];
+};
+
+export default {Filters, TEST_DATA, rand, generIcons, getRandomOffers, startTrip, finishAndDuration};
