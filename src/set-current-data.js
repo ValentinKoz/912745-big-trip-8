e1b71b13be574d2022d1e27318eb102e842082f9
Valentin Kozhin <kozhin_val@mail.ru>
2019-03-16 01:24:00 +0300

    капитан ООП

--- src/set-current-data.js
+++ src/set-current-data.js
@@ -4,7 +4,7 @@ import {rand} from './random.js';
 const currentData = (data) => ({
   routeType: generType(data.routeType),
   cities: data.cities[rand(7, 0)],
-  photo: data.photo,
+  photo: data.photo + `${Math.random()}`,
   offers: setPriceToOffers(getRandomOffers(data.offers)),
   text: getText(data.text),
   dateTrip: data.dateTrip,
